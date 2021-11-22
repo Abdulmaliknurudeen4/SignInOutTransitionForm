32210384e0f4b2f8334e4df12e2cf525d4db9fa0
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2021-11-22 03:02:13 +0100

    Create Styles For Transparent Screens

--- src/main/java/com/example/signinouttransitionform/HelloApplication.java
+++ src/main/java/com/example/signinouttransitionform/HelloApplication.java
@@ -3,17 +3,24 @@ package com.example.signinouttransitionform;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    public static Stage stage = null;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        this.stage = stage;
         stage.show();
     }
 
