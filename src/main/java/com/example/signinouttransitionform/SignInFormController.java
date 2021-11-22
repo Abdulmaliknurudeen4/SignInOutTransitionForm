6deb4e58239b02a35709f23e12d286f8b8bef590
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2021-11-22 03:03:28 +0100

    Work on SignInDesign

--- src/main/java/com/example/signinouttransitionform/SignInFormController.java
+++ src/main/java/com/example/signinouttransitionform/SignInFormController.java
@@ -1,4 +1,13 @@
 package com.example.signinouttransitionform;
 
-public class SignInFormController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignInFormController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
