fd0145628a1f3a8233762d62ade259a9ac9d0822
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2021-11-22 03:02:43 +0100

    Update Modules to Fix Errors.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,9 +6,13 @@ module com.example.signinouttransitionform {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires validatorfx;
+    requires org.kordamp.ikonli.core;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires org.kordamp.ikonli.fontawesome;
+    requires org.kordamp.ikonli.fontawesome5;
     requires eu.hansolo.tilesfx;
+
 
     opens com.example.signinouttransitionform to javafx.fxml;
     exports com.example.signinouttransitionform;
